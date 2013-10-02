b6df816a6ce218433980293292e6eb6d9ad6394c
yarlstel <yarlstel@gmail.com>
2013-10-02 12:42:08 +1200

    Hello Data is working

--- gen/ru/yaroslavstelmakh/hellodata/R.java
+++ gen/ru/yaroslavstelmakh/hellodata/R.java
@@ -10,6 +10,9 @@ package ru.yaroslavstelmakh.hellodata;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background_color=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -21,16 +24,19 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int data=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090002;
+        public static final int imageButton1=0x7f090000;
+        public static final int textView1=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -59,10 +65,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/ru/yaroslavstelmakh/hellodata/MainActivity.java
+++ src/ru/yaroslavstelmakh/hellodata/MainActivity.java
@@ -3,13 +3,17 @@ package ru.yaroslavstelmakh.hellodata;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);        
+        setContentView(R.layout.activity_main);        
+        tvHello = (TextView) findViewById(R.id.textView1);
     }
 
 
@@ -19,5 +23,8 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    public void onClick(View v) {
+		tvHello.setText("Hello Data!");
+	}
     
 }
