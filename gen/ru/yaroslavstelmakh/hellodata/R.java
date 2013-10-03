427b1b9ed0891cda05397e70ffc827b37c4fe70d
yarlstel <yarlstel@gmail.com>
2013-10-04 00:22:38 +1200

    add about.xml

--- gen/ru/yaroslavstelmakh/hellodata/R.java
+++ gen/ru/yaroslavstelmakh/hellodata/R.java
@@ -31,16 +31,17 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
-        public static final int butCatsCounter=0x7f090005;
-        public static final int butCrowsCounter=0x7f090004;
-        public static final int butGreen=0x7f090008;
-        public static final int butHello=0x7f090003;
-        public static final int butRed=0x7f090006;
-        public static final int butYellow=0x7f090007;
-        public static final int imageButton1=0x7f090002;
-        public static final int relativelayout=0x7f090000;
-        public static final int textView1=0x7f090001;
+        public static final int about_content=0x7f090000;
+        public static final int action_settings=0x7f09000a;
+        public static final int butCatsCounter=0x7f090006;
+        public static final int butCrowsCounter=0x7f090005;
+        public static final int butGreen=0x7f090009;
+        public static final int butHello=0x7f090004;
+        public static final int butRed=0x7f090007;
+        public static final int butYellow=0x7f090008;
+        public static final int imageButton1=0x7f090003;
+        public static final int relativelayout=0x7f090001;
+        public static final int textView1=0x7f090002;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -52,6 +53,8 @@ public final class R {
     public static final class string {
         public static final int Hello_Data=0x7f060005;
         public static final int Hello_Data2=0x7f060006;
+        public static final int about_text=0x7f06000d;
+        public static final int about_title=0x7f06000e;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int butCatsCounter_string=0x7f060008;
